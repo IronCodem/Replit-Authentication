6742fed9f48b515e84a7371db61d7eae520c5712
Yashas Shah <79524299+Tr1angular@users.noreply.github.com>
2021-06-05 21:35:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-    if (req.header('X-Replit-User-Id')) { // Check to see if the user is logged in...
+    if (req.header('X-Replit-User-Id')) {
         res.redirect(`/home/?user=${req.header('X-Replit-User-Name')}`); 
     } else {
         res.sendFile(__dirname + 'inlog.html'); 
@@ -12,11 +12,10 @@ app.get('/', function (req, res) {
 });
 
 app.get('/home', function (req, res) {
-    // You could do anything you want here...
-    // You might want to secure it a little, so people have to be logged in to view the page.
     res.send(`<h1>Hello, ${req.query.user}</h1>`);
 });
 
 app.listen(8080, function () { 
-    console.log('Server up!');
+    console.log("Server, up n\' runnin\'!");
 });
+
